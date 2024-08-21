b47fd1473de13b3902c66a4204de9ff78d18796f
James Beuzeval <james.beuzeval@gmail.com>
2024-08-21 19:06:10 +0100

    styling changes

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def display_deck(deck_df):
         if idx < len(deck_df):  # If there's data for this column
             row = deck_df.iloc[idx]
             with cols[idx]:
-                st.image(row['picture_url'], caption=row['hero_name'], width=100)
+                st.image(row['picture_url'], caption=row['hero name'], width=100)
                 st.write(f"Stars: {row['hero_stars']}")
                 st.write(f"Rank: {row['current_rank']}")
                 st.write(f"Gliding Score: {row['gliding_score']}")
@@ -257,23 +257,30 @@ def apply_table_styling():
         .sticky-header {
             position: sticky;
             top: 0;
-            z-index: 10;
+            z-index: 4; /* Ensure the header is above other elements */
             background-color: inherit; /* Match background color with the rest of the page */
             padding-top: 10px;
             padding-bottom: 10px;
             margin-bottom: 20px; /* Added margin to prevent overlap */
+        }
+                
+        .dataframe thead tr:first-child {
+            z-index: 6 !important; /* Ensure the first column is above the sticky header */
+            position: sticky;
         }
 
         /* Styling for the DataFrame container */
         .dataframe-container {
             max-height: calc(100vh - 200px); /* Adjust height dynamically */
             overflow-y: auto;
+            overflow-x: auto; /* Allow horizontal scrolling */
         }
 
         /* Table styling */
         .dataframe {
             border-collapse: separate;
             border-spacing: 0;
+            width: 100%;
         }
         .dataframe th, .dataframe td {
             padding: 8px;
@@ -283,12 +290,22 @@ def apply_table_styling():
         .dataframe th {
             position: sticky;
             top: 0;
-            z-index: 1;
+            z-index: 3; /* Ensure the header is above other elements */
             background-color: #f1f1f1;
             color: #333;
         }
+
+        /* Make the first column sticky */
+        .dataframe td:first-child, .dataframe th:first-child {
+            position: sticky;
+            left: 0;
+            z-index: 5; /* Keep the first column below the header */
+            background-color: #f9f9f9; /* Ensure the sticky column has a background */
+        }
         </style>
     """, unsafe_allow_html=True)
+
+
 
 
 def apply_hover_highlight():
@@ -409,7 +426,11 @@ def apply_hover_highlight():
 
 # Load your data
 all_heroes_df = pd.read_csv(DATA_FOLDER + '/allHeroData.csv', dtype={'hero_id': str})
+# Replace underscores with spaces in column headers
+all_heroes_df.columns = all_heroes_df.columns.str.replace('_', ' ')
+
 portfolio_df = pd.read_csv(DATA_FOLDER + '/portfolio.csv', dtype={'hero_id': str})
+portfolio_df.columns = portfolio_df.columns.str.replace('_', ' ')
 
 
 ###########################
@@ -417,7 +438,7 @@ portfolio_df = pd.read_csv(DATA_FOLDER + '/portfolio.csv', dtype={'hero_id': str
 ###########################
 
 # 1. Calculate the star frequency
-star_counts = all_heroes_df['hero_stars'].value_counts().sort_index(ascending=False)
+star_counts = all_heroes_df['hero stars'].value_counts().sort_index(ascending=False)
 star_cumsum = star_counts.cumsum()
 
 # 2. Define gradient colors (adjust to match your app's aesthetic)
@@ -435,8 +456,8 @@ gradient_colors = {
 def generate_css_styles(df, star_cumsum, gradient_colors):
     styles = []
     for i, row in df.iterrows():
-        rank = row['current_rank']
-        stars = row['hero_stars']
+        rank = row['current rank']
+        stars = row['hero stars']
         for star_level, cutoff in star_cumsum.items():
             if rank <= cutoff:
                 styles.append(f'background-color: {gradient_colors[star_level]};')
@@ -463,13 +484,13 @@ def style_dataframe_with_gradients(df, star_cumsum, gradient_colors):
 # Create a new column with HTML for images in both DataFrames
 def create_profile_image_links(df):
     return df.apply(
-        lambda x: f'<a href="https://fantasy.top/hero/{x["hero_handle"]}" target="_blank"><img src="{x["hero_profile_image_url"]}" width="50"></a>',
+        lambda x: f'<a href="https://fantasy.top/hero/{x["hero handle"]}" target="_blank"><img src="{x["hero profile image url"]}" class="round-img" width="50"></a>',
         axis=1
     )
 
 def create_card_image_links(df):
     return df.apply(
-        lambda x: f'<a href="https://fantasy.top/hero/{x["hero_handle"]}" target="_blank"><img src="{x["picture_url"]}" width="60"></a>',
+        lambda x: f'<a href="https://fantasy.top/hero/{x["hero handle"]}" target="_blank"><img src="{x["picture url"]}" width="60"></a>',
         axis=1
     )
 
@@ -477,36 +498,36 @@ all_heroes_df['Profile Image'] = create_profile_image_links(all_heroes_df)
 portfolio_df['Card Image'] = create_card_image_links(portfolio_df)
 
 # Drop the 'hero_profile_image_url' column as it's no longer needed
-all_heroes_df = all_heroes_df.drop(columns=['hero_profile_image_url'])
-portfolio_df = portfolio_df.drop(columns=['hero_profile_image_url'])
+all_heroes_df = all_heroes_df.drop(columns=['hero profile image url'])
+portfolio_df = portfolio_df.drop(columns=['hero profile image url'])
 
 # Define your column groups for all_heroes_df
 all_heroes_column_groups = {
-    'Basic Info': ['Profile Image', 'hero_name', 'hero_handle'],
-    'Current Fantasy': ['current_rank', 'fantasy_score'],
-    'Stars': ['hero_stars'],
-    'Supply': ['inflation_degree', 'rarity1Count', 'rarity2Count', 'rarity3Count', 'rarity4Count'],
-    'Social Stats': ['tweet_count', 'hero_followers_count', 'hero_fantasy_score', 'hero_views'],
-    'Market Data': ['rarity1_lowest_price', 'rarity2_lowest_price', 'rarity3_lowest_price', 'rarity4_lowest_price'],
+    'Basic Info': ['Profile Image', 'hero name', 'hero_handle'],
+    'Current Fantasy': ['current rank', 'fantasy score'],
+    'Stars': ['hero stars'],
+    'Supply': ['inflation degree', 'rarity1Count', 'rarity2Count', 'rarity3Count', 'rarity4Count'],
+    'Social Stats': ['tweet count', 'hero followers count', 'hero fantasy score', 'hero views'],
+    'Market Data': ['rarity1 lowest price', 'rarity2 lowest price', 'rarity3 lowest price', 'rarity4 lowest price'],
     'Last Trade Data': ['rarity1lastSalePrice', 'rarity2lastSalePrice', 'rarity3lastSalePrice','rarity4lastSalePrice'],
-    'Tournament Averages': ['Average', 'Main_Tournaments_Ave', 'Main_Last_4_Ave'],
-    'Tournament Variances': ['Variance', 'Main_Tournaments_Variance', 'Main_Last_4_Variance'],
-    'Tournament Standard Deviations': ['Standard_Deviation', 'Main_Tournaments_Standard_Deviation', 'Main_Last_4_Standard_Deviation'],
-    'Tournament Z-Scores': ['Z_Score', 'Main_Tournaments_Z_Score', 'Main_Last_4_Z_Score'],
-    'Value Analysis Bronze':  ['Price_to_Performance_Bronze', 'Coefficient_of_Variation_Bronze', 'Adjusted_Price_to_Performance_Bronze', 'Market_Relative_Price_to_Perf_Bronze', 'Adj_Price_to_Performance_Rank_Bronze'],
-    'Value Analysis Silver':  ['Price_to_Performance_Silver', 'Coefficient_of_Variation_Silver', 'Adjusted_Price_to_Performance_Silver', 'Market_Relative_Price_to_Perf_Silver', 'Adj_Price_to_Performance_Rank_Silver']
+    'Tournament Averages': ['Average', 'Main Tournaments Ave', 'Main Last 4 Ave'],
+    'Tournament Variances': ['Variance', 'Main Tournaments Variance', 'Main Last 4 Variance'],
+    'Tournament Standard Deviations': ['Standard Deviation', 'Main Tournaments Standard Deviation', 'Main Last 4 Standard Deviation'],
+    'Tournament Z-Scores': ['Z Score', 'Main Tournaments Z Score', 'Main Last 4 Z Score'],
+    'Value Analysis Bronze':  ['Price to Performance Bronze', 'Coefficient of Variation Bronze', 'Adjusted Price to Performance Bronze', 'Market Relative Price to Perf Bronze', 'Adj Price to Performance Rank Bronze'],
+    'Value Analysis Silver':  ['Price to Performance Silver', 'Coefficient of Variation Silver', 'Adjusted Price to Performance Silver', 'Market Relative Price to Perf Silver', 'Adj Price to Performance Rank Silver']
 }
 
 
 # Define your column groups for portfolio_df
 portfolio_column_groups = {
-    'Portfolio Info': ['Card Image', 'hero_name', 'hero_handle', 'rarity'],
-    'Current Fantasy': ['current_rank', 'fantasy_score', 'gliding_score'],
-    'Ownership': ['cards_number', 'listed_cards_number', 'in_deck'],
-    'Stars': ['hero_stars'],
+    'Portfolio Info': ['Card Image'],
+    'Current Fantasy': ['current rank', 'fantasy score', 'gliding score'],
+    'Ownership': ['cards number', 'listed cards number', 'in deck'],
+    'Stars': ['hero stars'],
     'Market Values': ['lowestPrice', 'lastSalePrice', 'rarityCount', 'Total Value'],
-    'Social Stats': ['hero_followers_count', 'hero_views'],
-    'Tournament Averages': ['Average', 'Main_Tournaments_Ave', 'Main_Last_4_Ave']
+    'Social Stats': ['hero followers count', 'hero views'],
+    'Tournament Averages': ['Average', 'Main Tournaments Ave', 'Main Last 4 Ave']
 }
 
 def handle_filters_and_sorting(df, column_groups, default_sort_column, default_sort_ascending):
@@ -522,10 +543,10 @@ def handle_filters_and_sorting(df, column_groups, default_sort_column, default_s
 
         # Filters for stars and values
         with col2:
-            if 'hero_stars' in df.columns:
+            if 'hero stars' in df.columns:
                 st.write("**Hero Stars Filter**")
-                selected_stars = st.slider('Hero Stars', min_value=int(df['hero_stars'].min()), max_value=int(df['hero_stars'].max()), value=(int(df['hero_stars'].min()), int(df['hero_stars'].max())))
-                df = df[(df['hero_stars'] >= selected_stars[0]) & (df['hero_stars'] <= selected_stars[1])]
+                selected_stars = st.slider('Hero Stars', min_value=int(df['hero stars'].min()), max_value=int(df['hero stars'].max()), value=(int(df['hero stars'].min()), int(df['hero stars'].max())))
+                df = df[(df['hero stars'] >= selected_stars[0]) & (df['hero stars'] <= selected_stars[1])]
 
         with col3:
             if 'Total Value Lowest Price' in df.columns:
@@ -542,7 +563,7 @@ def handle_filters_and_sorting(df, column_groups, default_sort_column, default_s
     return df
 
 # Create two main columns: one for content and one for the "right-hand sidebar"
-col_main, col_sidebar = st.columns([3, 1])
+col_main, col_sidebar = st.columns([4, 1])
 
 # Right-hand RSS Feed Sidebar
 with col_sidebar:
@@ -559,18 +580,18 @@ with col_main:
     if page_selection == "Portfolio Data":
         column_groups = portfolio_column_groups
         df = portfolio_df
-        default_sort_column = 'gliding_score'
+        default_sort_column = 'gliding score'
         default_sort_ascending = False
 
-        df['Total Value Lowest Price'] = df['lowestPrice'] * df['cards_number']
-        df['Total Value Last Sale Price'] = df['lastSalePrice'] * df['cards_number']
+        df['Total Value Lowest Price'] = df['lowestPrice'] * df['cards number']
+        df['Total Value Last Sale Price'] = df['lastSalePrice'] * df['cards number']
 
         # Fill NaN values with 0 to ensure they are included in the filter
         df['Total Value Lowest Price'] = df['Total Value Lowest Price'].fillna(0)
 
         total_portfolio_value_lowest = df['Total Value Lowest Price'].sum()
         total_portfolio_value_last_sale = df['Total Value Last Sale Price'].sum()
-        total_cards_value = df['cards_number'].sum()
+        total_cards_value = df['cards number'].sum()
 
         if 'Total Value Lowest Price' not in portfolio_column_groups['Market Values']:
             portfolio_column_groups['Market Values'].append('Total Value Lowest Price')
@@ -619,9 +640,9 @@ with col_main:
         # Display the table with the sticky header
         st.markdown('<div class="dataframe-container">{}</div>'.format(filtered_df.to_html(escape=False, index=False)), unsafe_allow_html=True)
         
-    # For the "All Heroes" page
+    # All Heroes Page
     elif page_selection == "All Heroes":
-        
+                    
         st.markdown('<div class="sticky-header"><h2>All Heroes</h2></div>', unsafe_allow_html=True)
 
         column_groups = all_heroes_column_groups
@@ -632,11 +653,8 @@ with col_main:
             df = df.drop(columns=['Total Value Lowest Price'])
             print("'Total Value Lowest Price' column was removed.")
 
-        default_sort_column = 'current_rank'
+        default_sort_column = 'current rank'
         default_sort_ascending = True
-
-        # Apply sorting
-        df = df.sort_values(by=default_sort_column, ascending=default_sort_ascending)
 
         # Apply filters and sorting   
         st.sidebar.subheader("Table Customisation")
@@ -664,13 +682,16 @@ with col_main:
         selected_columns = [col for col in selected_columns if col in df.columns]
         filtered_df = df[selected_columns]
 
-        print(filtered_df.head())
+        # Handle filtering and sorting
+        filtered_df = handle_filters_and_sorting(filtered_df, column_groups, default_sort_column, default_sort_ascending)
 
+        # Apply gradient styling after filtering and sorting
         styled_html = style_dataframe_with_gradients(filtered_df, star_cumsum, gradient_colors)
         
         apply_table_styling()
         apply_hover_highlight()
 
+        # Display the styled table with the sticky header
         st.markdown(f'<div class="dataframe-container">{styled_html}</div>', unsafe_allow_html=True)
 
         
@@ -684,25 +705,25 @@ with col_main:
             col1, col2, col3 = st.columns(3)
 
             with col1:
-                min_star, max_star = int(filtered_df['hero_stars'].min()), int(filtered_df['hero_stars'].max())
+                min_star, max_star = int(filtered_df['hero stars'].min()), int(filtered_df['hero stars'].max())
                 selected_stars = st.slider('Filter by Hero Stars', min_value=min_star, max_value=max_star, value=(min_star, max_star))
-                filtered_df = filtered_df[(filtered_df['hero_stars'] >= selected_stars[0]) & (filtered_df['hero_stars'] <= selected_stars[1])]
+                filtered_df = filtered_df[(filtered_df['hero stars'] >= selected_stars[0]) & (filtered_df['hero stars'] <= selected_stars[1])]
 
             with col2:
                 min_price, max_price = st.slider(
                     'Price Range',
-                    min_value=float(filtered_df['rarity4_lowest_price'].min()),
-                    max_value=float(filtered_df['rarity4_lowest_price'].max()),
-                    value=(float(filtered_df['rarity4_lowest_price'].min()), float(filtered_df['rarity4_lowest_price'].max())),
+                    min_value=float(filtered_df['rarity4 lowest price'].min()),
+                    max_value=float(filtered_df['rarity4 lowest price'].max()),
+                    value=(float(filtered_df['rarity4 lowest price'].min()), float(filtered_df['rarity4 lowest price'].max())),
                     step=0.001,
                     format="%.3f"
                 )
-                filtered_df = filtered_df[(filtered_df['rarity4_lowest_price'] >= min_price) & (filtered_df['rarity4_lowest_price'] <= max_price)]
+                filtered_df = filtered_df[(filtered_df['rarity4 lowest price'] >= min_price) & (filtered_df['rarity4 lowest price'] <= max_price)]
 
             with col3:
                 average_type = st.radio("Select average type for top 5", options=["Main_Tournaments_Ave", "Main_Last_4_Ave"])
 
-        selected_heroes = st.sidebar.multiselect('Select Heroes to Compare', options=filtered_df['hero_name'].unique())
+        selected_heroes = st.sidebar.multiselect('Select Heroes to Compare', options=filtered_df['hero name'].unique())
 
         tournament_columns = [
             'Main 11', 'Main 10', 'Main 9', 'Main 8', 'Main 7', 'Main 6 *Sat/Sun Only*',
@@ -711,19 +732,19 @@ with col_main:
         ]
 
         if not selected_heroes:
-            top_heroes = filtered_df.nlargest(5, average_type)['hero_name'].tolist()
+            top_heroes = filtered_df.nlargest(5, average_type)['hero name'].tolist()
         else:
             top_heroes = selected_heroes
 
-        long_df = filtered_df.melt(id_vars=['hero_name'], value_vars=tournament_columns,
+        long_df = filtered_df.melt(id_vars=['hero name'], value_vars=tournament_columns,
                                 var_name='Tournament', value_name='Points')
 
         fig = go.Figure()
 
-        for hero in long_df['hero_name'].unique():
-            hero_data = long_df[long_df['hero_name'] == hero]
+        for hero in long_df['hero name'].unique():
+            hero_data = long_df[long_df['hero name'] == hero]
 
-            hero_customdata = filtered_df[filtered_df['hero_name'] == hero][['rarity1_lowest_price', 'rarity2_lowest_price', 'rarity3_lowest_price', 'rarity4_lowest_price']].iloc[0].values
+            hero_customdata = filtered_df[filtered_df['hero name'] == hero][['rarity1_lowest_price', 'rarity2_lowest_price', 'rarity3_lowest_price', 'rarity4_lowest_price']].iloc[0].values
 
             opacity = 1.0 if hero in top_heroes else 0.2
 
