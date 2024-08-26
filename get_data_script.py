eb7b2785d814591e366451567874e8b62d6f1958
James Beuzeval <james.beuzeval@gmail.com>
2024-08-26 14:22:19 +0100

    Attempts to fix webdriver issues

--- get_data_script.py
+++ get_data_script.py
@@ -173,6 +173,7 @@ def setup_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+    print(ChromeDriverManager().install())
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     return driver
